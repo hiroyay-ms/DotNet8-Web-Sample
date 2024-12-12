70978b377d21c861071f5f01e78ec271f2cce73e
Hiroya Yamamoto <hiroyay@microsoft.com>
2024-12-12 10:49:37 +0900

    Update Web

--- src/Web/Pages/Users/Blob.cshtml.cs
+++ src/Web/Pages/Users/Blob.cshtml.cs
@@ -19,8 +19,32 @@ public class BlobModel : PageModel
         _logger = logger;
     }
 
+    [BindProperty(SupportsGet = true)]
+    public string? userId {get; set;} = string.Empty;
+
+    [BindProperty(SupportsGet = true)]
+    public string? containerName { get; set; } = string.Empty;
+
     public void OnGet()
     {
+        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(containerName)) {
+            ViewData["message"] = "Please provide the user id and container name";
+            return;
+        }
 
+        var filename = $"HelloWorld_{DateTime.Now.ToString("yyyyMMddHHmm")}_web.csv";
+
+        var storageAccountName = System.Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_NAME") ?? throw new InvalidOperationException("Connection string 'STORAGE_ACCOUNT_NAME' not found.");
+        DefaultAzureCredential credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userId });
+
+        BlobServiceClient blobServiceClient = new BlobServiceClient(new System.Uri($"https://{storageAccountName}.blob.core.windows.net"), credential);
+        BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName);
+
+        BlobClient blobClient = container.GetBlobClient(filename);
+
+        string text = $"Hello World from {userId}";
+        blobClient.Upload(BinaryData.FromString(text), overwrite: true);
+
+        ViewData["message"] = $"The file was successfully uploaded on blob storage with name {filename}";
     }
 }
